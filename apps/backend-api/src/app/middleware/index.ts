97219c46119ce4d5aeb3b63455c712d8142b9f1c
av3000 <alanas.vaiciulis@gmail.com>
2023-05-25 18:52:38 +0300

    chore: remove leftovers

--- apps/backend-api/src/app/middleware/index.ts
+++ apps/backend-api/src/app/middleware/index.ts
@@ -1,7 +1,3 @@
-const handleResponse = (res, data) => res.status(200).send(data);
-const handleError = (res, err: any = {}) =>
-  res.status(err.status || 500).send({ error: err.message });
-
 module.exports = {
   asyncErrorHandler: (fn) => (req, res, next) =>
     Promise.resolve(fn(req, res, next)).catch((error) => {
--- apps/backend-api/src/app/routes/carplates.ts
+++ apps/backend-api/src/app/routes/carplates.ts
@@ -11,19 +11,10 @@ const {
   findAll,
 } = require('../controllers/carplateController');
 
-// Create a new Tutorial
 router.post('/', asyncErrorHandler(create));
-
-// Retrieve all Tutorials
 router.get('/', findAll);
-
-// Retrieve a single Tutorial with id
 router.get('/:id', asyncErrorHandler(findOne));
-
-// Update a Tutorial with id
 router.put('/:id', asyncErrorHandler(update));
-
-// Delete a Tutorial with id
 router.delete('/:id', asyncErrorHandler(decomm));
 
 module.exports = router;
