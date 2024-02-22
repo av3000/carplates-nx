93461cc2bdb95160361fa96a1d6ca2e3e3979387
av3000 <alanas.vaiciulis@gmail.com>
2024-02-22 18:50:08 +0200

    swagger(feature): remove comment

--- apps/backend-api/src/app/utils/swagger.ts
+++ apps/backend-api/src/app/utils/swagger.ts
@@ -1,5 +1,3 @@
-// TODO: Make routes discoverable for swagger apis: []
-
 import { Express, Request, Response } from 'express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
