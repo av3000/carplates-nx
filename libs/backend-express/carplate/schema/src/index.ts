f3ef0dfd7e80af566f2174cea7947bdda57b90da
av3000 <alanas.vaiciulis@gmail.com>
2024-02-23 03:40:02 +0200

    feat: create backend-express carplate librare and move carplate schema

---
+++ libs/backend-express/carplate/schema/src/index.ts
@@ -0,0 +1,1 @@
+export * as CarplateModel from './carplate.model';
---
+++ libs/backend-express/utils/src/lib/mysql/database-connection.ts
@@ -0,0 +1,26 @@
+import { Sequelize, Dialect } from 'sequelize';
+import { mysqlConfig as dbConfig } from '../middleware/mysql.db';
+import CarplateModel from './carplate.model';
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect as Dialect,
+  port: parseInt(dbConfig.port),
+  operatorsAliases: {},
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+
+const CarplateSchema = CarplateModel(sequelize);
+
+const db = {
+  Sequelize,
+  sequelize,
+  CarplateSchema,
+};
+
+export default db;
