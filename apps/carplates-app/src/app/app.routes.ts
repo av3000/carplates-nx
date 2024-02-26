41adb26627b6b04bcf1b7c027ae10a811947ab16
av3000 <alanas.vaiciulis@gmail.com>
2024-02-26 23:16:10 +0200

    lint: remove redundant code bits

--- apps/carplates-app/src/app/app.routes.ts
+++ apps/carplates-app/src/app/app.routes.ts
@@ -1,18 +1,8 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'carplates',
     loadChildren: () =>
       import('@frontend-angular/carplate/carplate-feature-carplate-list').then(
         (m) => m.CarplateFeatureCarplateListModule
--- apps/carplates-app/src/app/home.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'carplates-home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent {}
