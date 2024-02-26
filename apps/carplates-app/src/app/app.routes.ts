227f921c0d57b50fd39a2b26efffd18d391e249c
av3000 <alanas.vaiciulis@gmail.com>
2024-02-26 23:10:13 +0200

    lint: remove unused carplate service

--- apps/carplates-app/src/app/app.routes.ts
+++ apps/carplates-app/src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'carplates',
     loadChildren: () =>
       import('@frontend-angular/carplate/carplate-feature-carplate-list').then(
         (m) => m.CarplateFeatureCarplateListModule
