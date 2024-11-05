2ad70ef671d06c335a54ad0dc93fe355dab889dc
av3000 <alanas.vaiciulis@gmail.com>
2024-11-05 15:37:16 +0100

    feat: add conditional for sentry to disable initialization for cypress

--- apps/backend-api/src/instrument.ts
+++ apps/backend-api/src/instrument.ts
@@ -2,18 +2,25 @@ import * as Sentry from '@sentry/node';
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 require('dotenv').config();
 
-// Ensure to call this before importing any other modules!
-Sentry.init({
-  dsn: process.env.SENTRY_EXPRESS_DSN,
-  integrations: [
-    // Add our Profiling integration
-    nodeProfilingIntegration(),
-  ],
-  // Add Tracing by setting tracesSampleRate
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
+const isSentryEnabled = process.env.SENTRY_ENABLED === 'true';
 
-  // Set sampling rate for profiling
-  // This is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+// Ensure to call this before importing any other modules!
+if (isSentryEnabled) {
+  Sentry.init({
+    dsn: process.env.SENTRY_EXPRESS_DSN,
+
+    integrations: [
+      // Add our Profiling integration
+      nodeProfilingIntegration(),
+    ],
+    // Add Tracing by setting tracesSampleRate
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+
+    // Set sampling rate for profiling
+    // This is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+} else {
+  console.log('Sentry is disabled for this environment.');
+}
